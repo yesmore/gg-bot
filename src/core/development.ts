b039329286d58c21f77ef981b613c8ce90eda5c9
yesmore <3224266014@qq.com>
2023-02-07 22:55:01 +0800

    6

--- src/core/development.ts
+++ src/core/development.ts
@@ -8,8 +8,8 @@ const development = async (bot: Telegraf<Context<Update>>) => {
   const botInfo = (await bot.telegram.getMe()).username;
 
   debug('Bot runs in development mode');
-  debug(`${botInfo} deleting webhook`);
-  await bot.telegram.deleteWebhook();
+  // debug(`${botInfo} deleting webhook`);
+  // await bot.telegram.deleteWebhook();
   debug(`${botInfo} starting polling`);
 
   await bot.launch();
