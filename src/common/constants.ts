b23ef3696df43361d730842ee84a314ac6775391
yesmore <3224266014@qq.com>
2023-02-07 21:17:50 +0800

    upd: add white list

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,10 @@
-export const White_List = ['大佬ddw', 'ddw', '大佬666', 'jbbdhs'];
+export const White_List = [
+  '大佬ddw',
+  'ddw',
+  '大佬666',
+  'jbbdhs',
+  'jb',
+  '卷b',
+  '卷逼',
+];
 export const White_List_Rule = new RegExp(`^(${White_List.join('|')})$`);
