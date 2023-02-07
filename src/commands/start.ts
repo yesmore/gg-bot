bf298546adfc6c8db53a1dde73b330094ff35723
songjunxi <songjunxi@fdsure.com>
2023-02-07 11:23:55 +0800

    upd: start cmd

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -6,7 +6,7 @@ import { author, name, version } from '../../package.json';
 const debug = createDebug('bot:about_command');
 
 const start = () => async (ctx: Context) => {
-  const message = `*欢迎使用唧唧bot小助手* \ncreated by ${author}\n\n/cmd 查看所有指令\n/help 查看帮助\n/about 关于唧唧bot`;
+  const message = `*🎉欢迎使用唧唧bot小助手🎉*\n\n/cmd 查看所有指令\n/help 查看帮助\n/about 关于唧唧bot`;
   debug(`Triggered "start" command with message \n${message}`);
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
 };
