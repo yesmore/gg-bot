7554af106538d9c20e0e0672263dd6d6c56bb9e4
songjunxi <songjunxi@fdsure.com>
2023-02-07 16:12:02 +0800

    6

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,21 @@ import { development, production } from './core';
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
+const randomPhoto = 'https://picsum.photos/200/300/?random';
+
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('start', start());
 bot.command('about', about());
-// message filter
+bot.command('cat', (ctx) => ctx.replyWithPhoto(randomPhoto));
 // bot.on('message', greeting());
 bot.on(message('new_chat_members'), greeting());
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-// custom cmdS
-bot.hears('/cmd', cmd_list());
+bot.hears(/\/list/, cmd_list());
 bot.hears('GG boy', (ctx) => ctx.reply('童话里做英雄'));
+bot.hears(/reverse (.+)/, (ctx) =>
+  ctx.reply(`reverse: ${ctx.match[1].split('').reverse().join('')}`)
+);
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
