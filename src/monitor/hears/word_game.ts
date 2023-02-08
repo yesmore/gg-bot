50a8d9777ab6ae0c54d2da9925e49e90c8d2f404
yesmore <3224266014@qq.com>
2023-02-08 21:04:02 +0800

    test game

--- src/monitor/hears/word_game.ts
+++ src/monitor/hears/word_game.ts
@@ -5,25 +5,24 @@ import { message } from 'telegraf/filters';
 export const word_game = () => async (ctx: Context) => {};
 
 const wordList = ['apple', 'banana', 'cherry'];
-const regex = new RegExp(`^@${bot.botInfo?.username}`);
+const regex = new RegExp(`^@${bot.botInfo?.username}\\s(\\w+)`);
 const scores = new Map();
 
 bot.hears('/word', (ctx) => {
   ctx.reply('Welcome to the Word Game! Start by guessing a word.');
 });
 
-bot.hears(regex, (ctx) => {
+bot.hears(/^@${bot.botInfo?.username}\\s(\\w+)/, (ctx) => {
   // if (!ctx.message.text.startsWith('@' + bot.botInfo?.username)) {
   //   return;
   // }
-
   let score = scores.get(ctx.from.id) || 0;
-  const guess = ctx.message.text.toLowerCase();
+  const guess = ctx.match[1].toLowerCase();
   const word = wordList[Math.floor(Math.random() * wordList.length)];
 
   if (guess === word) {
     score += 1;
-    ctx.reply(`Correct! The word was ${word}. Your score is now ${score}.`);
+    ctx.reply(`🎉Correct! The word was ${word}. Your score is now ${score}.`);
   } else {
     ctx.reply(`Incorrect. The word was ${word}. Your score is ${score}.`);
   }
