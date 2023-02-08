64a7bca870dc4111f789a5a02d496937c0c1bd22
songjunxi <songjunxi@fdsure.com>
2023-02-08 09:51:52 +0800

    test chatGPT

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Telegraf } from 'telegraf';
+import { Context, Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
+import { ChatGPTAPI } from 'chatgpt';
 import { about, start, list, photo } from './commands';
 import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
@@ -9,8 +10,10 @@ import { replyToMessage } from './utils';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY || '';
 
 const bot = new Telegraf(BOT_TOKEN);
+const api = new ChatGPTAPI({ apiKey: OPEN_AI_API_KEY });
 
 bot.command('start', start());
 bot.command('about', about());
@@ -26,6 +29,35 @@ bot.hears(White_List_Rule, (ctx) =>
 bot.hears(/r (.+)/, (ctx) =>
   ctx.reply(`reverse: ${ctx.match[1].split('').reverse().join('')}`)
 );
+bot.hears(/gpt (.+)/, (ctx) => {
+  const msg = ctx.match[1];
+  chatGpt(ctx, msg);
+});
+
+async function chatGpt(ctx: Context, msg: string) {
+  try {
+    await ctx.reply(`🤔正在组织语言，请稍等...`);
+    ctx.sendChatAction('typing');
+    const response = await api.sendMessage(msg);
+    console.log(
+      new Date().toLocaleString(),
+      '--AI response to <',
+      msg,
+      '>:',
+      response.text
+    );
+    await replyToMessage(ctx, ctx.message?.message_id!, response.text);
+    // ctx.editMessageText(response.text, {
+    //   chat_id: ctx.chat?.id,
+    //   message_id: ctx.message?.message_id,
+    //   parse_mode: 'Markdown',
+    // });
+  } catch (err) {
+    console.log('Error:', err);
+    await ctx.reply('😭出错了，请稍后再试；如果您是管理员，请检查日志。');
+    throw err;
+  }
+}
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
