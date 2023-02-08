2359ccdebfb94a5ca14da33e3837aaaa31b43283
yesmore <3224266014@qq.com>
2023-02-08 21:39:49 +0800

    chore

--- src/monitor/commands/word_game.ts
+++ src/monitor/commands/word_game.ts
@@ -1,28 +1,43 @@
 import bot from '../../bot';
+import { replyToMessage } from '../../utils';
 
+const isStart = false;
 const wordList = ['apple', 'banana', 'cherry'];
 const regex = new RegExp(`^@${bot.botInfo?.username} (.+)`);
 const scores = new Map();
 
+console.log(bot.botInfo);
 bot.command('guess', (ctx) => {
-  ctx.reply('Welcome to the Word Game! Start by guessing a word.');
+  ctx.reply(`欢迎来到猜词游戏!\n\n猜词格式：@${bot.botInfo?.username} xxx`);
 });
 
-bot.hears(/@akajs_bot (.+)/, (ctx) => {
+bot.hears(regex, (ctx) => {
   // if (!ctx.message.text.startsWith('@' + bot.botInfo?.username)) {
   //   return;
   // }
-  console.log(bot.botInfo);
 
-  let score = scores.get(ctx.from.id) || 0;
-  const guess = ctx.match[1].toLowerCase();
-  const word = wordList[Math.floor(Math.random() * wordList.length)];
+  if (isStart) {
+    let score = scores.get(ctx.from.id) || 0;
+    const guess = ctx.match[1].toLowerCase();
+    console.log(guess);
+    const word = wordList[Math.floor(Math.random() * wordList.length)];
 
-  if (guess === word) {
-    score += 1;
-    ctx.reply(`🎉Correct! The word was ${word}. Your score is now ${score}.`);
+    if (guess === word) {
+      score += 1;
+      replyToMessage(
+        ctx,
+        ctx.message?.message_id,
+        `🎉正确! 答案是 ${word}. 你的当前积分为 ${score}.`
+      );
+    } else {
+      replyToMessage(
+        ctx,
+        ctx.message?.message_id,
+        `错误. 答案是 ${word}. 你的当前积分为 ${score}.`
+      );
+    }
+    scores.set(ctx.from.id, score);
   } else {
-    ctx.reply(`Incorrect. The word was ${word}. Your score is ${score}.`);
+    ctx.reply(`请发送 /guess 开始猜词游戏~`);
   }
-  scores.set(ctx.from.id, score);
 });
