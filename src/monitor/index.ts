79b7ad6ae63314c65ef729bc5b68770ca2d5d779
yesmore <3224266014@qq.com>
2023-02-11 14:26:00 +0800

    1

--- src/monitor/index.ts
+++ src/monitor/index.ts
@@ -25,10 +25,10 @@ bot.hears(/\/r (.+)/, (ctx) =>
 );
 bot.hears(/\/ai (.+)/, async (ctx) => {
   const msg = ctx.match[1];
-  ctx.reply(`🤔正在组织语言...`);
-  // ctx.sendChatAction('typing');
+  // ctx.reply(`🤔正在组织语言...`);
+  ctx.sendChatAction('typing');
   const res = await sendAiRequest(msg);
-  replyToMessage(ctx, ctx.message.message_id, res);
+  await replyToMessage(ctx, ctx.message.message_id, res);
 });
 bot.hears('gg boy', gg_boy);
 bot.hears(White_List_Rule, be_yourself);
